0362f35bda2b4048ed15840eabbf6d8300525e4d
Oriol Mauri <oriol@bmauri.com>
2021-06-27 12:26:02 +0200

    Food Icons

--- src/components/Icons/Food.jsx
+++ src/components/Icons/Food.jsx
@@ -10,10 +10,10 @@ const Food = ({
       { icon?.up == 0 && <img src={Up0} alt="icon"  />}
       { icon?.up == 1 && <img src={Up1} alt="icon" />}
       { icon?.up == 2 && <img src={Up2} alt="icon" />}
-      { icon?.level == 1 && <img src={Food1} alt="icon" id="Food1" /> }
-      { icon?.level == 2 && <img src={Food2} alt="icon" id="Food2" /> }
-      { icon?.level == 3 && <img src={Food3} alt="icon" id="Food3" /> }
-      { icon?.level == 4 && <img src={Food4} alt="icon" id="Food4" /> }
+      { icon?.level == 1 && <img src={Food1} alt="icon" id="Food1" className="food" /> }
+      { icon?.level == 2 && <img src={Food2} alt="icon" id="Food2" className="food" /> }
+      { icon?.level == 3 && <img src={Food3} alt="icon" id="Food3" className="food" /> }
+      { icon?.level == 4 && <img src={Food4} alt="icon" id="Food4" className="food" /> }
       { icon?.down == 0 && <img src={Down0} alt="icon" />}
       { icon?.down == 1 && <img src={Down1} alt="icon" />}
       { icon?.down == 2 && <img src={Down2} alt="icon" />}
